9f2804e2a9da9b14a8df87d9f1f16de6f054b678
hayssamchebli-chh <hayssam.chebli@chhholding.com>
2026-07-14 10:10:03 +0300

    Update abb_cat_auto_app.py

--- abb_cat_auto_app.py
+++ abb_cat_auto_app.py
@@ -1067,14 +1067,14 @@ with input_col1:
     with manual_type_col:
         types_text = st.text_area(
             "Type",
-            height=220,
-            placeholder="Example:\nMotion Sensor\nSwitch 2G",
+            height=200,
+            placeholder="Example:\nMotion Sensor\n\nSwitch 2G",
         )
 
     with manual_code_col:
         codes_text = st.text_area(
             "Code",
-            height=220,
+            height=200,
             placeholder="Example:\nZW213\nTZ107\nABB-TZW510",
         )
 
